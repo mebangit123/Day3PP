5ed5d0883e80818db2420523ba6ec29082869955
mebangit123 <mebannongrum333@gmail.com>
2021-04-15 11:48:41 +0530

    Using compareTo method added to main branch

--- LineComparison.java
+++ LineComparison.java
@@ -5,16 +5,10 @@ public class LineComparison {
 		int x1=1, y1=2, x2=4, y2=3;
 		int x3=3, y3=5, x4=2, y4=1;
 
-		double line1 = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2 - y1, 2));
-		double line2 = Math.sqrt(Math.pow(x4-x3, 2) + Math.pow(y4 - y3, 2));
-		
-		if (line1 == line2)
-		{
-			System.out.println("Equals");
-		}
-		else
-		{
-			System.out.println("Not Equal");
-		}
+		String line1 = Double.toString(Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2 - y1, 2)));
+		String line2 = Double.toString(Math.sqrt(Math.pow(x4-x3, 2) + Math.pow(y4 - y3, 2)));
+
+		System.out.println(line1.equals(line2));
+		System.out.println(line1.compareTo(line2));
 	}
 }
