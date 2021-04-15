d0ab896a19d993f1b000cfede1fc2bc712ac678b
mebangit123 <mebannongrum333@gmail.com>
2021-04-15 10:28:56 +0530

    Logic for checking equality of two lines added to main branch

--- LineComparison.java
+++ LineComparison.java
@@ -3,8 +3,18 @@ public class LineComparison {
 		System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
 		
 		int x1=1, y1=2, x2=4, y2=3;
-		int length = (int)Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2 - y1, 2));
-		System.out.println(length);
+		int x3=3, y3=5, x4=2, y4=1;
+
+		double line1 = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2 - y1, 2));
+		double line2 = Math.sqrt(Math.pow(x4-x3, 2) + Math.pow(y4 - y3, 2));
 		
+		if (line1 == line2)
+		{
+			System.out.println("Equals");
+		}
+		else
+		{
+			System.out.println("Not Equal");
+		}
 	}
 }
